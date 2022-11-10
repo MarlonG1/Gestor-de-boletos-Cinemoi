b6efc713826bf5b5a501fb32bdfbfdfb19ce298b
Brian Chavez <Brian.Chavez@catolica.edu.sv>
2022-11-10 07:56:58 -0600

    Diseño Pre-eliminar

--- Proyecto_Cinemoi/Cartelera/Cartelera.Designer.cs
+++ Proyecto_Cinemoi/Cartelera/Cartelera.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cineMoiLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ElpadrinoPictureBox = new System.Windows.Forms.PictureBox();
+            this.SuperMariopicturebox = new System.Windows.Forms.PictureBox();
+            this.AvengerPicturebox = new System.Windows.Forms.PictureBox();
+            this.cineMoiPicrutebox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ElpadrinoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SuperMariopicturebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AvengerPicturebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cineMoiPicrutebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(822, 211);
+            this.panel1.TabIndex = 0;
+            // 
+            // cineMoiLabel
+            // 
+            this.cineMoiLabel.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cineMoiLabel.Location = new System.Drawing.Point(342, 40);
+            this.cineMoiLabel.Name = "cineMoiLabel";
+            this.cineMoiLabel.Size = new System.Drawing.Size(147, 28);
+            this.cineMoiLabel.TabIndex = 3;
+            this.cineMoiLabel.Text = "cineMoi";
+            this.cineMoiLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.ElpadrinoPictureBox);
+            this.panel2.Controls.Add(this.SuperMariopicturebox);
+            this.panel2.Controls.Add(this.AvengerPicturebox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 330);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(822, 212);
+            this.panel2.TabIndex = 1;
+            // 
+            // ElpadrinoPictureBox
+            // 
+            this.ElpadrinoPictureBox.Image = global::Proyecto_Cinemoi.Properties.Resources.ElpadrinoPelicula;
+            this.ElpadrinoPictureBox.Location = new System.Drawing.Point(598, 37);
+            this.ElpadrinoPictureBox.Name = "ElpadrinoPictureBox";
+            this.ElpadrinoPictureBox.Size = new System.Drawing.Size(133, 163);
+            this.ElpadrinoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ElpadrinoPictureBox.TabIndex = 4;
+            this.ElpadrinoPictureBox.TabStop = false;
+            // 
+            // SuperMariopicturebox
+            // 
+            this.SuperMariopicturebox.Image = global::Proyecto_Cinemoi.Properties.Resources.Super_mario_encartelera;
+            this.SuperMariopicturebox.Location = new System.Drawing.Point(347, 89);
+            this.SuperMariopicturebox.Name = "SuperMariopicturebox";
+            this.SuperMariopicturebox.Size = new System.Drawing.Size(142, 111);
+            this.SuperMariopicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SuperMariopicturebox.TabIndex = 5;
+            this.SuperMariopicturebox.TabStop = false;
+            // 
+            // AvengerPicturebox
+            // 
+            this.AvengerPicturebox.Image = global::Proyecto_Cinemoi.Properties.Resources.Avenger_endgame;
+            this.AvengerPicturebox.Location = new System.Drawing.Point(115, 37);
+            this.AvengerPicturebox.Name = "AvengerPicturebox";
+            this.AvengerPicturebox.Size = new System.Drawing.Size(135, 150);
+            this.AvengerPicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AvengerPicturebox.TabIndex = 6;
+            this.AvengerPicturebox.TabStop = false;
+            // 
+            // cineMoiPicrutebox
+            // 
+            this.cineMoiPicrutebox.Image = global::Proyecto_Cinemoi.Properties.Resources.Cinemoi_Logo;
+            this.cineMoiPicrutebox.Location = new System.Drawing.Point(12, 30);
+            this.cineMoiPicrutebox.Name = "cineMoiPicrutebox";
+            this.cineMoiPicrutebox.Size = new System.Drawing.Size(78, 59);
+            this.cineMoiPicrutebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cineMoiPicrutebox.TabIndex = 2;
+            this.cineMoiPicrutebox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 28);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Estrenos";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(822, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Cartelera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(822, 542);
+            this.Controls.Add(this.cineMoiLabel);
+            this.Controls.Add(this.cineMoiPicrutebox);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Cartelera";
+            this.Text = "Cartelera";
+            this.Load += new System.EventHandler(this.Cartelera_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ElpadrinoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SuperMariopicturebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AvengerPicturebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cineMoiPicrutebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox cineMoiPicrutebox;
+        public System.Windows.Forms.Label cineMoiLabel;
+        private System.Windows.Forms.PictureBox AvengerPicturebox;
+        private System.Windows.Forms.PictureBox SuperMariopicturebox;
+        private System.Windows.Forms.PictureBox ElpadrinoPictureBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label label1;
     }
 }
 
